487f8e5ee22bfc703918e54a32360b458b9c1c9c
Elio Lu <1520622848@qq.com>
2021-10-04 14:05:28 +0800

    upd: node endpoints

--- src/server.js
+++ src/server.js
@@ -1,25 +1,38 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 
-// const articlesInfo = {
-//     'learn-react': {
-//         upvotes: 0
-//     },
-//     'learn-node': {
-//         upvotes: 0,
-//     },
-//     'my-thoughts-on-resumes': {
-//         upvotes: 0
-//     }
-// }
+const articlesInfo = {
+    'learn-react': {
+        upvotes: 0,
+        comments: []
+    },
+    'learn-node': {
+        upvotes: 0,
+        comments: []
+    },
+    'my-thoughts-on-resumes': {
+        upvotes: 0,
+        comments: []
+    }
+}
 
 const app = express()
 
 // TODO: deprecated
 app.use(bodyParser.json())
 
-app.get('/hello', (req, res) => res.send('hello'))
-app.get('/hello/:name', (req, res) => res.send(`hello ${req.params.name}`))
-app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}`))
+// add endpoints
+app.post('/api/articles/:name/upvote', (req, res) => {
+    const articleName = req.params.name
+    articlesInfo[articleName].upvotes++
+    res.status(200).send(`${articleName} has been voted ${articlesInfo[articleName].upvotes} !`)
+})
+
+app.post('/api/articles/:name/add-comment', (req, res) => {
+    const { username, text } = req.body
+    const articleName = req.params.name
+    articlesInfo[articleName].comments.push({ username, text })
+    res.status(200).send(articlesInfo[articleName])
+})
 
 app.listen(8000, () => console.log('Listening on port 8000'))
